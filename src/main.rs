fb30f7574358e63ab6482ba513590c02dab69b53
Kevin Matthew Kent <kevin_kent@pop-os.localdomain>
2022-11-19 13:43:56 -0500

    incorporating references

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use ndarray::{Array2};
 fn main() {
    let args: Vec<String> = env::args().collect();
    let l = args[1].parse::<u16>().unwrap();
-   let total = prob_total(l);
-   let res = normalized_prob(0, 0, l, total);
-   let prob_array = make_probability_array(l);
+   let total = prob_total(&l);
+   let res = normalized_prob(0, 0, &l, &total);
+   let prob_array = make_probability_array(&l);
    let mut starting_arr = Array2::<f64>::zeros((l as usize, l as usize));
    println!("prob: {}", prob_array[[0, 0]])
 }
--- src/make_spark.rs
+++ src/make_spark.rs
@@ -4,7 +4,7 @@ use itertools::iproduct;
 //use imageproc::region_labelling::{connected_components, Connectivity};
 use ndarray::{Array2};
 
-fn prob_spark(i: i16, j: i16, l: u16) -> f64 {
+fn prob_spark(i: i16, j: i16, l: &u16) -> f64 {
     let l = l/10;
     let base: f64 = E;
     let i_use: i16 = i + 1;
@@ -12,31 +12,31 @@ fn prob_spark(i: i16, j: i16, l: u16) -> f64 {
     return base.powf(-i_use as f64/l as f64)*base.powf(-j_use as f64/l as f64);
 }
 
-pub fn prob_total(l: u16) -> f64 {
+pub fn prob_total(l: &u16) -> f64 {
     let mut total: f64 = 0.0;
-    for (i_test, j_test) in iproduct!(0..l, 0..l){
+    for (i_test, j_test) in iproduct!(0..*l, 0..*l){
         total += prob_spark(i_test as i16, j_test as i16, l);
     }
     return total
 
 }
 
-pub fn normalized_prob(i: i16, j: i16, l: u16, total: f64) -> f64 {
+pub fn normalized_prob(i: i16, j: i16, l: &u16, total: &f64) -> f64 {
 
-    return prob_spark(i, j, l)/total
+    return prob_spark(i, j, &l)/total
 
 }
 
-pub fn make_probability_array(l: u16) -> Array2::<f64> {
+pub fn make_probability_array(l: &u16) -> Array2::<f64> {
 
-    let mut prob_array = Array2::<f64>::zeros((l as usize, l as usize));
+    let mut prob_array = Array2::<f64>::zeros((*l as usize, *l as usize));
 
     let total = prob_total(l);
     let mut prob_result = 0.0;
 
-    for (i_test, j_test) in iproduct!(0..l, 0..l){
+    for (i_test, j_test) in iproduct!(0..*l, 0..*l){
         
-        prob_result = normalized_prob(i_test as i16, j_test as i16, l, total);
+        prob_result = normalized_prob(i_test as i16, j_test as i16, &l, &total);
         prob_array[[i_test as usize, j_test as usize]] = prob_result
     }
 

898cc92ff671602c765aecdb6508173f6b7cfcb0
Kevin Matthew Kent <kevin_kent@pop-os.localdomain>
2022-11-19 13:33:44 -0500

    just need to implement connected components

--- src/make_spark.rs
+++ src/make_spark.rs
@@ -1,8 +1,10 @@
 //mod std::f64::consts;
 use std::f64::consts::E;
 use itertools::iproduct; 
+//use imageproc::region_labelling::{connected_components, Connectivity};
+use ndarray::{Array2};
 
-pub fn prob_spark(i: i16, j: i16, l: u16) -> f64 {
+fn prob_spark(i: i16, j: i16, l: u16) -> f64 {
     let l = l/10;
     let base: f64 = E;
     let i_use: i16 = i + 1;
@@ -22,6 +24,23 @@ pub fn prob_total(l: u16) -> f64 {
 pub fn normalized_prob(i: i16, j: i16, l: u16, total: f64) -> f64 {
 
     return prob_spark(i, j, l)/total
+
+}
+
+pub fn make_probability_array(l: u16) -> Array2::<f64> {
+
+    let mut prob_array = Array2::<f64>::zeros((l as usize, l as usize));
+
+    let total = prob_total(l);
+    let mut prob_result = 0.0;
+
+    for (i_test, j_test) in iproduct!(0..l, 0..l){
+        
+        prob_result = normalized_prob(i_test as i16, j_test as i16, l, total);
+        prob_array[[i_test as usize, j_test as usize]] = prob_result
+    }
+
+    prob_array
 
 }
 
